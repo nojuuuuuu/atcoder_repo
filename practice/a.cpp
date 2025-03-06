5b9587428d87b498e712218029fe39fee3ce4d89
nojuuuuuu <uuunoj@gmail.com>
2025-03-06 11:04:24 +0900

    貪欲法-おつり

--- practice/a.cpp
+++ practice/a.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 int main(){
+    int price;
+    cin >> price;
+    int change = 1000 - price;
 
+    vector<int> a = {500, 100, 50, 10, 5, 1};
 
+    int sum = 0;
+
+    for (int i=0; i<6; i++){
+        int num = change / a[i];
+        sum += num;
+        change -= num*a[i];
+    }
+
+    cout << sum << endl;
     return 0;
 }
